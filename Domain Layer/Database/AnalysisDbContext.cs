29bb0c0319c3d5083ef81df30e78f10c4b331919
pralad raya <parladrayamajhi89@gmail.com>
2026-05-03 11:28:44 +1000

    Add EF Core SQL Server support and AnalysisDbContext setup
    Integrated Entity Framework Core with SQL Server in Analysis Web.
    Configured AnalysisDbContext and connection string. Added required
    NuGet packages to Domain Layer. Removed placeholder Class1.cs and
    added folder structure for future development.

---
+++ Domain Layer/Database/AnalysisDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain_Layer.Database
+{
+    public class AnalysisDbContext : DbContext
+    {
+        public AnalysisDbContext(DbContextOptions<AnalysisDbContext> options) : base(options)
+        {
+        }
+
+        protected AnalysisDbContext()
+        {
+        }
+    }
+}
